211f81f8bcdeb56bbefbd4f70efa73b6f94e37ce
William Luo <gluo7777@gmail.com>
2018-10-20 17:32:18 -0500

    thymeleaf mapping changes, moved some logic to controller
    - moved request url extraction to controller instead of in view to reduce repetition

--- src/main/java/com/ceruleanmind/controllers/SiteController.java
+++ src/main/java/com/ceruleanmind/controllers/SiteController.java
@@ -1,17 +1,27 @@
 package com.ceruleanmind.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SiteController {
+	
 	@GetMapping("/")
 	public String cover() {
 		return "redirect:/home";
 	}
 	
 	@GetMapping(path = {"/home","/projects","/apps","/ideas","/contact","/examples"})
-	public String pages() {
+	public String pages(HttpServletRequest request, Model model) {
+		StringBuffer requestURL = request.getRequestURL();
+		int start = requestURL.lastIndexOf("/") + 1;
+		if(start < requestURL.length()) {
+			String pageName = requestURL.substring(start);
+			model.addAttribute(pageName+"Active", true);
+		}
 		return "index";
 	}
 }
